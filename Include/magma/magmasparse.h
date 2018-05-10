9da536afca414f73a2096128fee3888a6dff8a5e
Theodore <tlcfem@gmail.com>
2018-05-11 01:57:19 +1200

    ADD MAGMA SPARSE HEADERS

---
+++ Include/magma/magmasparse.h
@@ -0,0 +1,40 @@
+/*
+    -- MAGMA (version 2.3.0) --
+       Univ. of Tennessee, Knoxville
+       Univ. of California, Berkeley
+       Univ. of Colorado, Denver
+       @date November 2017
+*/
+
+#ifndef MAGMASPARSE_H
+#define MAGMASPARSE_H
+
+/* ------------------------------------------------------------
+ * MAGMASPARSE BLAS Functions
+ * --------------------------------------------------------- */
+//#include "magmasparseblas.h"
+
+/* ------------------------------------------------------------
+ * MAGMASPARSE functions
+ * --------------------------------------------------------- */
+#include "magmasparse_c.h"
+#include "magmasparse_d.h"
+#include "magmasparse_s.h"
+#include "magmasparse_z.h"
+
+// mixed precision
+#include "magmasparse_ds.h"
+#include "magmasparse_zc.h"
+
+/* ------------------------------------------------------------
+ * MAGMASPARSE types
+ * --------------------------------------------------------- */
+/*
+#include "magmasparse_types_c.h"
+#include "magmasparse_types_d.h"
+#include "magmasparse_types_s.h"
+#include "magmasparse_types_z.h"
+
+*/
+#include "magmasparse_types.h"
+#endif /* MAGMASPARSE_H */
